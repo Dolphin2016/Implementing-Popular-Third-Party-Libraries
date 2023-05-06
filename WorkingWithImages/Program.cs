adcc9a2d9e4350572f72a25a8eacec78f66b2408
Dolphin2016 <evgenij55555@gmail.com>
2023-05-06 22:30:51 +0500

    Generating Grayscale Thumbnails

--- WorkingWithImages/Program.cs
+++ WorkingWithImages/Program.cs
@@ -1,1 +1,41 @@
 ﻿
+string imagesFolder = Path.Combine(
+    Environment.CurrentDirectory, "images");
+
+WriteLine($"I will look for images in the following folder:\n{imagesFolder}");
+WriteLine();
+
+if (!Directory.Exists(imagesFolder))
+{
+    WriteLine();
+    WriteLine("Folder does not exists!");
+    return;
+}
+
+IEnumerable<string> images =
+    Directory.EnumerateFiles(imagesFolder);
+
+foreach (string imagePath in  images)
+{
+    if (Path.GetFileNameWithoutExtension(imagePath).EndsWith("-thumbnail"))
+    {
+        WriteLine($"Skippint:\n {imagePath}");
+        WriteLine();
+        continue; // this file has already been converted
+    }
+
+    string thumbnailPath = Path.Combine(
+        Environment.CurrentDirectory, "images",
+        Path.GetFileNameWithoutExtension(imagePath)
+        + "-thumbnail" + Path.GetExtension(imagePath));
+
+    using (Image image = Image.Load(imagePath))
+    {
+        WriteLine($"Converting:\n {imagePath}");
+        WriteLine($"To:\n {thumbnailPath}");
+        image.Mutate(x => x.Resize(image.Width / 10, image.Height / 10));
+        image.Mutate(x => x.Grayscale());
+        image.Save(thumbnailPath);
+        WriteLine();
+    }
+}

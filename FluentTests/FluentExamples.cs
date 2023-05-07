0fcbff01f60178a5e26260e088d9e4fcc6fb1462
Dolphin2016 <evgenij55555@gmail.com>
2023-05-07 11:45:53 +0500

    Making Fluent Assertions about Collections and Arrays

--- FluentTests/FluentExamples.cs
+++ FluentTests/FluentExamples.cs
@@ -22,5 +22,16 @@ namespace FluentTests
 
             city.Length.Should().Be(6);
         }
+
+        [Fact]
+        public void TestCollections()
+        {
+            string[] names = new[] { "Alice", "Bob", "Charly" };
+
+            names.Should().HaveCountLessThan(4,
+                "because the maximum items should be 3 or fewer");
+
+            names.Should().OnlyContain(name => name.Length <= 6);
+        }
     }
 }

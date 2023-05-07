2eaefee1fa909d3bc1682f36a768e8e342afb4f5
Dolphin2016 <evgenij55555@gmail.com>
2023-05-07 11:54:24 +0500

    Making Fluent Assertions about Dates and Times

--- FluentTests/FluentExamples.cs
+++ FluentTests/FluentExamples.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using FluentAssertions.Extensions;
 
 namespace FluentTests
 {
@@ -32,6 +33,28 @@ namespace FluentTests
                 "because the maximum items should be 3 or fewer");
 
             names.Should().OnlyContain(name => name.Length <= 6);
+        }
+
+        [Fact]
+        public void TestDateTimes()
+        {
+            DateTime when = new(
+                hour: 9, minute: 30, second: 0,
+                day: 25, month: 3, year: 2023);
+
+            when.Should().Be(25.March(2023).At(9, 30));
+
+            when.Should().BeOnOrAfter(23.March(2023));
+
+            when.Should().NotBeSameDateAs(12.February(2023));
+
+            when.Should().HaveYear(2023);
+
+            DateTime due = new(
+                hour: 13, minute: 0, second: 0,
+                day: 25, month: 3, year: 2023);
+
+            when.Should().BeAtLeast(2.Hours()).Before(due);
         }
     }
 }

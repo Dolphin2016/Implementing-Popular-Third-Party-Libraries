d756067fdb0948ac406314a7791c9aa99f20ffe5
Dolphin2016 <evgenij55555@gmail.com>
2023-05-07 13:06:55 +0500

    Validating with an Empty Order

---
+++ FluentValidation.App/Program.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation.Models; // Order
+using FluentValidation.Validators; // OrderValidation
+using FluentValidation.Results; // ValidationResult
+
+Order order = new()
+{
+    // start with an invalid order
+};
+
+OrderValidator validator = new();
+
+ValidationResult result = validator.Validate(order);
+
+WriteLine($"CustomerName:   {order.CustomerName}");
+WriteLine($"CustomerEmail:  {order.CustomerEmail}");
+WriteLine($"CusomerLevel:   {order.CustomerLevel}");
+WriteLine($"OrderId:        {order.OrderId}");
+WriteLine($"OrderDate:      {order.OrderDate}");
+WriteLine($"ShipDate:       {order.ShipDate}");
+WriteLine($"Total:          {order.Total}");
+WriteLine();
+WriteLine($"IsValid:        {result.IsValid}");
+foreach( var item in result.Errors)
+{
+    WriteLine($" {item.Severity}: {item.ErrorMessage}\n");
+}
